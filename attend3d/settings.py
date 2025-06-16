ab95f8dbba0f233a5ffd9222a7bd61f0e44f1839
phong <phongnguyen.050503@gmail.com>
2025-06-16 22:55:41 +0700

    FC0002

--- attend3d/settings.py
+++ attend3d/settings.py
@@ -161,8 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zephyrnguyen.vn@gmail.com'
-EMAIL_HOST_PASSWORD = 'uhad fopw pxmb ptlc'   
+EMAIL_HOST_USER = '[your email]'
+EMAIL_HOST_PASSWORD = '[your password app]'   
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MEDIA_URL = '/media/'
